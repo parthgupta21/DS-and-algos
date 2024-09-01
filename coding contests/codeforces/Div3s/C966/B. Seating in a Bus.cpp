bae80b14515983a461c5ac3c31cec8cc30b59610
Parth <20parthgupta03@gmail.com>
2024-09-01 11:46:30 +0530

    practising contest

---
+++ coding contests/codeforces/Div3s/C966/B. Seating in a Bus.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define f(i, a, b) for (auto i = (a); i < (b); ++i)
+#define rep(i, n) f(i, 0, n)
+#define rf(i, a, b) for (auto i = (a); i >= (b); --i)
+#define rrep(i, n) rf(i, (n)-1, 0)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define print_1D_arr(a, n) for (long long i = 0; i < n; i++) { cout << a[i] << ' '; } cout << endl;
+#define in_1D_arr(a,n) f(i,0,n){cin>>a[i];}
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef long long int lli;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<vector<ll>> vvll;
+typedef vector<bool> vbol;
+const ll mod = 1e9 + 7;
+void solve() {
+    lli n;
+    cin >> n;
+
+    lli a[n];
+    in_1D_arr(a, n);
+
+    lli freq[n + 1];
+
+    memset(freq, 0, sizeof(freq));
+    freq[a[0]] = 1;
+
+    for (int i = 1; i < n; i++){
+
+        if(freq[a[i] + 1] != 1 and freq[a[i] - 1] != 1){
+            cout << "No" << endl;
+            return;
+        }
+
+        freq[a[i]]++;
+
+    }
+
+
+    cout << "Yes" << endl;
+}
+int main() {
+    lli t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
